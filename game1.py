60c9dc882c6f67ae800cc77fff4cde5f4c46b4bc
Ruri <akizuki.ruri@gmail.com>
2019-08-01 23:20:31 +0900

    Controlling added.

--- game1.py
+++ game1.py
@@ -1,33 +1,60 @@
 import pygame
 from pygame.locals import *
-import sys
+from sys import exit
 
 # Color defination
 white = (255, 255, 255)
 black = (0, 0, 0)
+skyblue = (135, 206, 250)
 
 
-# Function defination
+# Class defination
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = pygame.Surface((100, 100))
+        self.surf = pygame.Surface((20, 20))
         self.surf.fill(white)
         self.rect = self.surf.get_rect()
-player = Player()
 
+    def update(self, pressed_keys):
+        self.d = 10
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -self.d)
+            print('up pressed')
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, self.d)
+            print('down pressed')
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-self.d, 0)
+            print('left pressed')
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(self.d, 0)
+            print('right pressed')
 
-# Initialization
+# Objects initialization
 pygame.init()
-screen = pygame.display.set_mode((1600, 900))
+screen = pygame.display.set_mode((1080, 720))
+screen.fill(black)
+bg = pygame.Surface(screen.get_size())
+bg.fill(skyblue)
+player = Player()
+clock = pygame.time.Clock()
 
 # Main Window
 while 1:
     for event in pygame.event.get():
         if event.type == QUIT:
-            sys.exit()
+            exit()
 
-# Blit
-    screen.fill(black)
-    screen.blit(player.surf, (775, 425))
+# Move
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+
+# Visualization
+    screen.blit(bg, (0, 0))
+    screen.blit(player.surf, player.rect)
     pygame.display.flip()
+
+# FPS controlling
+    print(clock.tick())
+    clock.tick(60)
